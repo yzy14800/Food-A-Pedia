4a3bcebe4eae57bedc4977d5bb3be54714e99e5b
candicelkf <lkfcp3@gmail.com>
2016-04-23 16:50:45 -0400

    Update index.php
    Add text animation

--- font/index.php
+++ font/index.php
@@ -8,7 +8,6 @@
 <script src="js/jquery.min.js"></script>
 <!-- Custom Theme files -->
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-
 <!-- Custom Theme files -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
